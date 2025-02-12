684848b55215c3d4fe8cb7d62fb4304a07b8d950
TotalUmbrella <jerryruoxuzhang@gmail.com>
2025-02-12 16:16:57 +0800

    working !!
    dynamic routes are working
    
    next is to figure out css styling for markdown and such.

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,26 @@
 import Image from "next/image";
 import Link from "next/link";
-import { getMarkdownNames } from "./markdown";
+import { getMarkdownNames } from "../pages/api/markdown";
+
+const array = await getMarkdownNames();
 
 export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen">
       <div className="flex flex-col items-left justify-between min-h-screen py-2 w-2/5">
         <header className="font-[Ubuntu] text-2xl width-full flex flex-row items-center mt-2 mb-4 justify-first font-normal height-10">
-          $ ls ~/totalumbrel.la <span className="cursor"></span>
+          <Link href="/" className="header">
+            $ ls ~/totalumbrel.la
+          </Link>
+          <span className="cursor"></span>
         </header>
         <main className="width-full flex-grow flex flex-col items-left justify-first py-5">
           words and stuff blah blah blah
           <br/>
           <br/>
           ill write more later
-          <Link href="/test">testing link</Link>
           <ul className="list-disc pl-5">
+            {array.map((name) =>  <li key={name}><Link className="link" href={`/${name}`}>{name}</Link></li>)}
           </ul>
         </main>
         <footer className="mb-5 width-full flex flex-row items-center justify-between text-lg font-medium">
---
+++ pages/[name].tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import "../app/globals.css"
+import Link from "next/link";
+
+
+export default function Page() {
+    const router = useRouter();
+    const name = router.query.name;
+    const [metadata, setMetadata] = useState(null);
+    const [contentHtml, setContentHtml] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    useEffect(() => {
+      fetch(`/api/markdown?name=${name}`)
+        .then((res) => res.json())
+        .then((data) => {
+          setMetadata(data.metadata);
+          setContentHtml(data.contentHtml);
+          setIsLoading(false);
+        });
+    }, [name]);
+
+    if (isLoading) {
+      return <p className="w-full h-full flex justify-center items-center"></p>;
+    }
+    else if (!contentHtml) {
+      return <p></p>;
+    }
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+      <div className="flex flex-col items-left justify-between min-h-screen py-2 w-2/5">
+        <header className="font-[Ubuntu] text-2xl width-full flex flex-row items-center mt-2 mb-4 justify-first font-normal height-10">
+          <Link href="/" className="header">
+            $ ls ~/totalumbrel.la
+          </Link>
+          <span className="cursor"></span>
+        </header>
+        <main className="width-full flex-grow flex flex-col items-left justify-first py-5">
+          <div className="markdownbody" dangerouslySetInnerHTML={{ __html: contentHtml }}></div>
+        </main>
+        <footer className="mb-5 width-full flex flex-row items-center justify-between text-lg font-medium">
+        <div>
+         © Jerry Zhang
+        </div>
+        <div>
+          gyatt
+        </div>
+        </footer>
+      </div>
+    </div>
+    );
+  }
