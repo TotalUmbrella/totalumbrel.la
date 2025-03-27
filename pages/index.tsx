8624d65d756152047c202258bb4b84accf8a7d09
TotalUmbrella <jerryruoxuzhang@gmail.com>
2025-03-27 11:20:26 +0800

    a

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,11 @@ import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
 import '../app/globals.css';
-const Home = () => {
+const Home = (filesMeta) => {
+  console.log(filesMeta);
+  //what the actual fuck??? i have no idea why filesMeta is wrapped around a smaller filesMeta? i cbb fixing tho so oh well
+  const projects = filesMeta.filesMeta.filter((file) => file.data.tags.includes("project"))
+  const misc = filesMeta.filesMeta.filter((file) => file.data.tags.includes("misc"))
   return (
     <div className="flex items-center justify-center min-h-screen">
       <div className="flex flex-col items-left justify-between min-h-screen py-2 w-2/5">
@@ -17,20 +21,17 @@ const Home = () => {
         <main className="width-full flex-grow flex flex-col items-left justify-first py-5">
           
           Hi. This is a website which aims to showcase and document things that I am making/have made.
-          <br/>
-          <br/>
+          <div className="spacer"></div>
           I'm passionate about a lot of things, including engineering and programming. 
-          <br/>
+          <div className="spacer"></div>
           you can take a look at the projects that I've been working on,
           <ul className="list-disc pl-5">
-            {/* {projects.map((name) =>  <li key={name[1].title}><Link className="link" href={`/${name[0]}`}>{name[1].title}</Link></li>)} */}
+            {projects.map((name) =>  <li key={name.data.title}><Link className="link" href={`/${name.filename}`}>{name.data.title}</Link></li>)}
           </ul>
-          <br/>
-          <br/>
-          <br/>
+          <div className="spacer"></div><div className="spacer"></div>
           or check out blog posts on some other miscellaneous stuff
           <ul className="list-disc pl-5">
-            {/* {misc.map((name) =>  <li key={name[1].title}><Link className="link" href={`/${name[0]}`}>{name[1].title}</Link></li>)} */}
+            {misc.map((name) =>  <li key={name.data.title}><Link className="link" href={`/${name.filename}`}>{name.data.title}</Link></li>)}
           </ul>
           <br/>
           I will continue to add things of interest in the future.
@@ -50,19 +51,20 @@ const Home = () => {
   );
 }
 
-// export const getStaticProps = async () => {
-//   const files = fs.readdirSync("items")
-//   const filesMeta = files.map((filename)=> {
-//     let contents =fs.readFileSync(path.join('items',filename+'.md')).toString();
-//     let parsedContents = matter(contents);
-//     return [parsedContents]
-//   });
-//   console.log(filesMeta)
-//   return {
-//     props: {
-//       filesMeta
-//     }
-//   }
-// }
+export const getStaticProps = async () => {
+  const files = fs.readdirSync("items").map(file => file.replace(/\.md$/, ""));
+  const filesMeta = files.map((filename)=> {
+    let contents =fs.readFileSync(path.join('items',filename+".md")).toString();
+    let parsedContents = matter(contents);
+    let data: { [key: string]: any } = parsedContents.data;
+    return {filename, data}
+  });
+  console.log(filesMeta[0].data.title)
+  return {
+    props: {
+      filesMeta
+    }
+  }
+}
 
 export default Home;
